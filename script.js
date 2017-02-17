29244a2fd976ee59efc2231a996470d234c0d453
Oleg Liulia <Nuclearman@me.com>
2017-02-17 18:33:20 +0200

    add basic files

---
+++ script.js
@@ -0,0 +1,73 @@
+"use strict"
+
+let data =
+{
+	next: "https://pokeapi.co/api/v2/pokemon",
+	pokemons: [],
+	loading: false,
+	details:
+	{
+		loading: false,
+		open: false,
+		name: "",
+		id: 0,
+		weight: 0,
+		height: 0,
+		base_experience: 0,
+		order: 0,
+	},
+}
+
+let methods =
+{
+	loadMore()
+	{
+		this.loading = true
+
+		fetch(this.next).then(r => r.json()).then(r =>
+		{
+			this.next = r.next
+			this.pokemons.push(...r.results)
+			this.loading = false
+		})
+	},
+	loadDetails(url)
+	{
+		let { details } = this
+		if (details.loading) return
+
+		details.loading = true
+		details.open = false
+
+		fetch(url).then(r => r.json()).then(r =>
+		{
+			Object.assign(details, r,
+			{
+				open: true,
+				loading: false,
+			})
+
+			scrollTo({ top: 0, behavior: "smooth" })
+		})
+	},
+	removePokemon(index)
+	{
+		this.pokemons.splice(index, 1)
+	},
+}
+
+let isVisible = entry => entry.intersectionRatio == 1
+let app = new Vue
+({
+	el: "#app",
+	data,
+	methods,
+	mounted()
+	{
+		new IntersectionObserver(entries =>
+		{
+			if (entries.some(isVisible)) this.loadMore()
+		})
+		.observe(this.$refs.loadButton)
+	},
+})
